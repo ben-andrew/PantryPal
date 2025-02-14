f4229cf3c4421a40a5ddfdfc855f27eda45802d1
Micah <micah.ramirez89@gmail.comgit config --global user.name Micahgit statushelpgit config --global user.email micah.ramirez89@gmail.com>
2025-02-14 17:01:53 -0500

    added api keys

---
+++ apiKeys.js
@@ -0,0 +1,7 @@
+export default keys = {
+    //the API anon key
+    anon: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdnbXZ1dGdraWxjbHB5aWZieGZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgzNDg0NzMsImV4cCI6MjA1MzkyNDQ3M30.s3t9Ossr3aruv_fLKvxXyf5Rl_e3x-oEpGq1mA_Ij8k,
+    
+    //the url
+    url: 'https://ggmvutgkilclpyifbxfx.supabase.co'
+};
