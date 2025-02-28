6db20d9f445a144bcc1472a514efa13973282926
lwyman2704 <153244844+lwyman2704@users.noreply.github.com>
2025-02-28 16:20:23 -0500

    Screens and other files
    I just uploaded my files so that you guys can see what I have so far because I have some slight problems with them running

--- App.js
+++ App.js
@@ -1,16 +1,35 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import PantryScreen from './screens/PantryScreen';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import SplashScreen from "./screens/splashscreen.js";
+import LoginScreen from "./screens/loginscreen.js";
+import Dashboard from './screens/dashboard.js';
+import 'react-native-gesture-handler';
+import { View, StyleSheet } from 'react-native';
+
 
 const Stack = createStackNavigator();
 
 export default function App() {
+  console.log("Current screen: Splash");
+  console.log(Dashboard);
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Pantry">
-        <Stack.Screen name="Pantry" component={PantryScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}> {/* Corrected line */}
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Splash" screenOptions={{ headerShown: true }}>
+          <Stack.Screen name="Splash" component={SplashScreen} />
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Dashboard" component={Dashboard} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red'
+  }
+});
